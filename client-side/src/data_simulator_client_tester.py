bc605fe12f1afc09ba9e41931f1fd79e64960022
Jonathan <jonathan.p20jp@gmail.com>
2025-08-28 18:05:13 -0700

    Simple simulator that goes on indefinitely adding unique users to our kafka topics

--- client-side/src/data_simulator_client_tester.py
+++ client-side/src/data_simulator_client_tester.py
@@ -1,12 +1,14 @@
 import UserInfo_pb2
 import UserInfo_pb2_grpc
 import grpc
+import time
 
 def main():
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = UserInfo_pb2_grpc.UserInfoStub(channel)
         # Test message
-        for i in range(100):
+        i = 0
+        while(True):
             user = UserInfo_pb2.User(userId=f"{i}",
                                     mobileNumber=f"+1 (555)-{i}",
                                     email=f"johndoe{i}@gmail.com",
@@ -20,6 +22,10 @@ def main():
                 print(f"Response was successful, and user was pushed to [topic: {transaction_response.kafkaTopic}, partition: {transaction_response.partition}]")
             else:
                 print("Not successful!")
+
+            i+=1
+            # Sleep for one second
+            time.sleep(1)
 
 
 if __name__ == "__main__":
